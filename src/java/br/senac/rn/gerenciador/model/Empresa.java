e33cfde81f46dba0008da4ac133c2f4fdc43950b
a9210433 <a9210433@02LAB108PC16.rn.senac.br>
2017-09-25 21:19:25 -0300

    Commit 25/09 Java Web

---
+++ src/java/br/senac/rn/gerenciador/model/Empresa.java
@@ -0,0 +1,68 @@
+package br.senac.rn.gerenciador.model;
+
+
+public class Empresa {
+    
+    private int id;
+    private String nome;
+
+    public Empresa() {
+    }
+
+    public Empresa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Empresa(String doceria_Bela_Vista) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empresa other = (Empresa) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "id=" + id + ", nome=" + nome + '}';
+    }
+    
+    
+}
